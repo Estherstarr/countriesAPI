7d67d7052c5fdb404204b54185c5d77895462abc
onomeh <onwuanibeonome@gmail.com>
2022-06-24 16:42:52 +0000

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="bg-[#2d4474] shadow-lg py-3 px-10 flex justify-between">
+      <h1 className="font-bold text-white">Where is the world?</h1>
+      <h3 className="text-white">Dark Mode</h3>
+    </div>
+  );
+}
